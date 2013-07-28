c02ddb337560369663f167417108cece4cfe55f2
Markus Maga <markus@nadilus.com>
2013-07-28 13:44:44 +0200

    rename to as4

---
+++ Assignment4/Assignment4/Program.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Markus Maga">
+//   AC7525 Markus Maga 21-07-13
+// </copyright>
+// <summary>
+//   The program startes the WinForm application.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Assignment3
+{
+    using System;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
